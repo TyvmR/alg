a923fd65d60518a5581d13548d07a2f3daeb20cf
tyvmr <tyvmr@163.com>
2023-04-09 08:44:55 +0800

    增加java的算法模块

---
+++ al-4/src/main/java/al/red4/App.java
@@ -0,0 +1,13 @@
+package al.red4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
