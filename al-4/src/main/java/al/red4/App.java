b972ba806d37f338f2be9f9e1a8c8c5ab1ab05d6
john.huang <john.huang@cloudwise.com>
2023-04-09 22:40:42 +0800

    binarysearch

--- al-4/src/main/java/al/red4/App.java
+++ al-4/src/main/java/al/red4/App.java
@@ -10,4 +10,17 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+
+    
+   /**
+    * 
+   * @Title: drawBasic 
+   * @Description: 基本绘图demo: 默认使用JavaFx
+   * @author Administrator
+   * @date 2023年4月9日下午9:45:38
+    */
+    public void drawBasic() {
+    	
+    }
+    
 }
